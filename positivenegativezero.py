a8a6c310493ee191ae439ed9c77844575110f657
Adhish-Ullasan <adhishullasan@gmail.com>
2025-09-15 19:32:41 +0530

    python basics : positive negative zero

---
+++ positivenegativezero.py
@@ -0,0 +1,8 @@
+a = int(input('Enter a Number:'))
+
+if a == 0 :
+    print("Entered number is Zero")
+elif a > 0 :
+    print("Entered number is Positive")
+else:
+    print("Entered number is  Negative")
